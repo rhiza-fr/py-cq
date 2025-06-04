89664ffc22b59a3d76cefb35bdf22a3f7b6f14cd
Chris Kilner <chris@radix.fr>
2025-06-04 19:06:39 +0200

    Prepare for bug fix

--- src/cq/parsers/compile_parser.py
+++ src/cq/parsers/compile_parser.py
@@ -1,14 +1,38 @@
-from pathlib import Path
-from typing import Dict
 from cq.localtypes import AbstractParser, RawResult, ToolResult
 
 
 class CompileParser(AbstractParser):
     def parse(self, raw_result: RawResult) -> ToolResult:
-        # For projects, compileall returns 0 even if some files fail
-        # So we need to parse the output to determine success
+        
+        # Listing '.\\src\\cq'...
+        # Listing '.\\src\\cq\\parsers'...
+        # Compiling '.\\data\\problems\\travelling_salesman\\ts_bad.py'...
+        # Compiling '.\\data\\problems\\travelling_salesman\\ts_good.py'...
+        # Compiling '.\\src\\cq\\__init__.py'...
+        # Compiling '.\\src\\cq\\cli.py'...
+        # Compiling '.\\src\\cq\\config.py'...
+        # Compiling '.\\src\\cq\\execution_engine.py'...
+        # Compiling '.\\src\\cq\\localtypes.py'...
+        # Compiling '.\\src\\cq\\main.py'...
+        # ***   File ".\data\problems\travelling_salesman\ts_bad.py", line 31
+        #     error = {a = b}
+        #              ^^^^^
+        # SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+
+        # Compiling '.\\src\\cq\\metric_aggregator.py'...
+        # Compiling '.\\src\\cq\\parsers\\__init__.py'...
+        # Compiling '.\\src\\cq\\parsers\\common.py'...
+        # Compiling '.\\src\\cq\\parsers\\complexity_parser.p
+
+        print(raw_result)
         success = True
-        failed_files: Dict[str, str] = {}
+        failed_files: dict[str, str] = {}
+        
+        # this error parsing is wrong. See the comment above
+        # Successfull compilation is marked with "Compiling"
+        # Skipped directories are marked "Listing" and can be ignored
+        # Failed compilation is marked *** File "filename", line linenumber
+        # .... then more error help until the end or the next Compiling or Listing or errror
         
         if raw_result.stderr:
             # Parse compileall error output
